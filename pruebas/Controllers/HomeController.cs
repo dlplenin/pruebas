5a25fee5b4eb3786b6df21b48b6037b1a3fd84fb
Diego Pardo <dlplenin@hotmail.com>
2020-08-29 00:56:39 -0500

    Add project files.

---
+++ pruebas/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using pruebas.Models;
+using Wkhtmltopdf.NetCore;
+using PdfSharp.Pdf.IO;
+using System.Drawing.Text;
+using System.Drawing;
+using PdfSharp.Fonts;
+
+namespace pruebas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            GeneratePDF2();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void GeneratePDF()
+        {
+            var customFamilies = new PrivateFontCollection();
+
+            customFamilies.AddFontFile(".\\Drift.ttf");
+            var family = customFamilies.Families[0];
+
+            var myfont = new Font(family, 40, FontStyle.Regular, GraphicsUnit.Point);
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            //XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+            XFont font = new XFont(myfont.FontFamily, 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+        }
+        
+        public void GeneratePDF2()
+        {
+            // That's all it takes to register your own fontresolver
+            GlobalFontSettings.FontResolver = new DemoFontResolver();
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            XFont font = new XFont("Drift", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+        }
+    }
+}
---
+++ pruebas/Models/DemoFontResolver.cs
@@ -0,0 +1,73 @@
+﻿using PdfSharp.Fonts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace pruebas.Models
+{
+    public class DemoFontResolver : IFontResolver
+    {
+        public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            // Ignore case of font names.
+            var name = familyName.ToLower();
+
+            // Deal with the fonts we know.
+            switch (name)
+            {
+                case "drift":
+                    return new FontResolverInfo("Drift Wood");
+            }
+
+            // We pass all other font requests to the default handler.
+            // When running on a web server without sufficient permission, you can return a default font at this stage.
+            return PlatformFontResolver.ResolveTypeface(familyName, isBold, isItalic);
+        }
+
+        /// <summary>
+        /// Return the font data for the fonts.
+        /// </summary>
+        public byte[] GetFont(string faceName)
+        {
+            switch (faceName)
+            {
+                case "Drift Wood":
+                    return DemoFontHelper.Drift;
+            }
+
+            //return GetFont(faceName);
+            return null;
+        }
+    }
+}
+
+/// <summary>
+/// Helper class that reads font data from embedded resources.
+/// </summary>
+public static class DemoFontHelper
+{
+    public static byte[] Drift
+    {
+        get { return LoadFontData("pruebas.Drift.ttf"); }
+    }
+
+    /// <summary>
+    /// Returns the specified font from an embedded resource.
+    /// </summary>
+    static byte[] LoadFontData(string name)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        using Stream stream = assembly.GetManifestResourceStream(name);
+        if (stream == null)
+            throw new ArgumentException("No resource with name " + name);
+
+        int count = (int)stream.Length;
+        byte[] data = new byte[count];
+        stream.Read(data, 0, count);
+        return data;
+    }
+}
